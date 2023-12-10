87c501aae81d0c6b168cb560d8fd08d8724c7a55
giezz <giezz@vk.com>
2023-12-10 15:51:53 +0300

    Модуль анкетирования пациента: Questionnaire.

---
+++ questionnaire/src/test/java/ru/rightcode/anketi/AnketiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rightcode.anketi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnketiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
