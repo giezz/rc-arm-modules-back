f507e9029fbd4d4173a2f3e1271cbb79217bdad7
Soundflog <paha-vershinin@mail.ru>
2024-01-06 15:29:17 +0300

    Добавлены DTO для Scale и Form

---
+++ questionnaire/src/main/java/ru/rightcode/anketi/DTO/ScaleDTO.java
@@ -0,0 +1,13 @@
+package ru.rightcode.anketi.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ScaleDTO {
+    private Long id;
+    private String name;
+    private String description;
+
+}
