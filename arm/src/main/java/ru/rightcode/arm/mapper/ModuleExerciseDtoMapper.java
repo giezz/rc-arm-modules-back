0a1f7cc2641bd4729ce180b74e2d79925c0cb1b9
giezz <giezz@vk.com>
2024-02-03 00:03:55 +0300

    Рефакторинг маппинга DTO.

---
+++ arm/src/main/java/ru/rightcode/arm/mapper/ModuleExerciseDtoMapper.java
@@ -0,0 +1,31 @@
+package ru.rightcode.arm.mapper;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import ru.rightcode.arm.dto.ModuleDto;
+import ru.rightcode.arm.dto.ModuleExerciseDto;
+import ru.rightcode.arm.model.Module;
+import ru.rightcode.arm.model.ModuleExercise;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class ModuleExerciseDtoMapper implements Mapper<ModuleExercise, ModuleExerciseDto> {
+
+    @Override
+    public ModuleExerciseDto map(ModuleExercise object) {
+        return new ModuleExerciseDto(
+                object.getId(),
+                object.getExercise().getId(),
+                object.getBlock().getId(),
+                object.getFinishedAt()
+        );
+    }
+
+    public List<ModuleExerciseDto> mapAll(Collection<ModuleExercise> moduleExercise) {
+        return moduleExercise.stream().map(this::map).collect(Collectors.toList());
+    }
+}
