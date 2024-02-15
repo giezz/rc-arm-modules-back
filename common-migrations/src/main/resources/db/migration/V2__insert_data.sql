8b2f756c32445830e2daf4283f45a09bb6c0e081
giezz <giezz@vk.com>
2024-02-15 16:18:44 +0300

    Рефакторинг БД (выравнивание столбцов таблиц).

--- common-migrations/src/main/resources/db/migration/V2__insert_data.sql
+++ common-migrations/src/main/resources/db/migration/V2__insert_data.sql
@@ -1,13 +1,36 @@
-INSERT INTO doc._user (id, login, password) VALUES (1, 'admin', '$2a$12$XM6RePLH/FcCB1yu3izFYOQRLaiYr/u8F2NrmJwBH/KJbpct1eJQu');
-INSERT INTO doc._user (id, login, password) VALUES (2, 'user', '$2a$12$Kwjwg2cajJef3S/8uCWPwuTg8PAXTDuJ9TPXZ66LVXpegbgSbvsGW');
+--
+-- Data for Name: _role; Type: TABLE DATA; Schema: doc; Owner: postgres
+--
 
 INSERT INTO doc._role (id, name) VALUES (1, 'ADMIN');
 INSERT INTO doc._role (id, name) VALUES (2, 'DOCTOR');
 
+--
+-- Data for Name: _user; Type: TABLE DATA; Schema: doc; Owner: postgres
+--
+
+INSERT INTO doc._user (id, login, password) VALUES (1, 'admin', '$2a$12$XM6RePLH/FcCB1yu3izFYOQRLaiYr/u8F2NrmJwBH/KJbpct1eJQu');
+INSERT INTO doc._user (id, login, password) VALUES (2, 'user', '$2a$12$Kwjwg2cajJef3S/8uCWPwuTg8PAXTDuJ9TPXZ66LVXpegbgSbvsGW');
+
+--
+-- Data for Name: user_role; Type: TABLE DATA; Schema: doc; Owner: postgres
+--
+
 INSERT INTO doc.user_role (_user_id, _role_id) VALUES (1, 2);
 INSERT INTO doc.user_role (_user_id, _role_id) VALUES (1, 1);
+INSERT INTO doc.user_role (_user_id, _role_id) VALUES (2, 1);
+INSERT INTO doc.user_role (_user_id, _role_id) VALUES (2, 2);
+
+--
+-- Data for Name: doctor; Type: TABLE DATA; Schema: doc; Owner: postgres
+--
 
 INSERT INTO doc.doctor (id, doctor_code, first_name, middle_name, last_name, email, phone_number, _user_id) VALUES (1, 100, 'Михаил', 'Алексеевич', 'Батухтин', 'giezz@vk.com', '777777', 1);
+INSERT INTO doc.doctor (id, doctor_code, first_name, middle_name, last_name, email, phone_number, _user_id) VALUES (2, 101, 'Тест', 'Тестович', 'Тестов', 'test@vk.com', '777771', 2);
+
+--
+-- Data for Name: passport; Type: TABLE DATA; Schema: doc; Owner: postgres
+--
 
 INSERT INTO doc.passport (id, series, number, issued, issued_date) VALUES (1, '2345', '678901', 'Отделение УФМС по г. Санкт-Петербургу', '2012-08-20');
 INSERT INTO doc.passport (id, series, number, issued, issued_date) VALUES (2, '3456', '789012', 'Отделение УФМС по г. Екатеринбургу', '2014-02-10');
@@ -15,12 +38,50 @@ INSERT INTO doc.passport (id, series, number, issued, issued_date) VALUES (3, '4
 INSERT INTO doc.passport (id, series, number, issued, issued_date) VALUES (4, '5678', '901234', 'Отделение УФМС по г. Казани', '2015-06-15');
 INSERT INTO doc.passport (id, series, number, issued, issued_date) VALUES (5, '6789', '12345 ', 'Отделение УФМС по г. Владивостоку', '2018-03-25');
 
+--
+-- Data for Name: patient_status; Type: TABLE DATA; Schema: doc; Owner: postgres
+--
+
 INSERT INTO doc.patient_status (id, name) VALUES (1, 'Нуждается в реабилитации');
 INSERT INTO doc.patient_status (id, name) VALUES (2, 'Проходит реабилитацию');
 INSERT INTO doc.patient_status (id, name) VALUES (3, 'Проходил реабилитацию ранее');
 
-INSERT INTO doc.patient (id, patient_code, first_name, middle_name, last_name, birth_date, death_date, address, phone_number, work_place_data, bookmark, snils, polis, patient_status_id, passport_id) VALUES (1, 1005, 'Елена', 'Петровна', 'Иванова', '1992-03-18', null, 'ул. Ленина, д. 20, кв. 8, Санкт-Петербург', '5551111', 'Газпром', null, '23456789012', '3456789012345678', 1, 1);
-INSERT INTO doc.patient (id, patient_code, first_name, middle_name, last_name, birth_date, death_date, address, phone_number, work_place_data, bookmark, snils, polis, patient_status_id, passport_id) VALUES (2, 1006, 'Андрей', 'Валерьевич', 'Сидоров', '1980-12-05', null, 'пр. Победы, д. 15, кв. 3, Екатеринбург', '5552222', 'Транснефть', null, '34567890123', '4567890123456789', 2, 2);
-INSERT INTO doc.patient (id, patient_code, first_name, middle_name, last_name, birth_date, death_date, address, phone_number, work_place_data, bookmark, snils, polis, patient_status_id, passport_id) VALUES (3, 1007, 'Ольга', 'Сергеевна', 'Кузнецова', '1975-06-30', null, 'ул. Гагарина, д. 8, кв. 12, Нижний Новгород', '5553333', 'РЖД', null, '45678901234', '5678901234567890', 1, 3);
-INSERT INTO doc.patient (id, patient_code, first_name, middle_name, last_name, birth_date, death_date, address, phone_number, work_place_data, bookmark, snils, polis, patient_status_id, passport_id) VALUES (4, 1008, 'Иван', 'Дмитриевич', 'Петров', '1988-09-10', null, 'пр. Ленинградский, д. 25, кв. 7, Казань', '5554444', 'Лукойл', null, '56789012345', '6789012345678901', 3, 4);
-INSERT INTO doc.patient (id, patient_code, first_name, middle_name, last_name, birth_date, death_date, address, phone_number, work_place_data, bookmark, snils, polis, patient_status_id, passport_id) VALUES (5, 1009, 'Мария', 'Андреевна', 'Новикова', '1995-04-27', null, 'ул. Красная, д. 18, кв. 6, Владивосток', '5555555', 'Сбербанк', null, '67890123456', '7890123456789012', 2, 5);
+--
+-- Data for Name: patient; Type: TABLE DATA; Schema: doc; Owner: postgres
+--
+
+INSERT INTO doc.patient (id, patient_code, first_name, middle_name, last_name, birth_date, death_date, address, phone_number, work_place_data, bookmark, snils, polis, patient_status_id, passport_id, doctor_id, _user_id) VALUES (4, 1008, 'Иван', 'Дмитриевич', 'Петров', '1988-09-10', NULL, 'пр. Ленинградский, д. 25, кв. 7, Казань', '5554444', 'Лукойл', NULL, '56789012345', '6789012345678901', 3, 4, NULL, 2);
+INSERT INTO doc.patient (id, patient_code, first_name, middle_name, last_name, birth_date, death_date, address, phone_number, work_place_data, bookmark, snils, polis, patient_status_id, passport_id, doctor_id, _user_id) VALUES (5, 1009, 'Мария', 'Андреевна', 'Новикова', '1995-04-27', NULL, 'ул. Красная, д. 18, кв. 6, Владивосток', '5555555', 'Сбербанк', NULL, '67890123456', '7890123456789012', 2, 5, NULL, 2);
+INSERT INTO doc.patient (id, patient_code, first_name, middle_name, last_name, birth_date, death_date, address, phone_number, work_place_data, bookmark, snils, polis, patient_status_id, passport_id, doctor_id, _user_id) VALUES (2, 1006, 'Андрей', 'Валерьевич', 'Сидоров', '1980-12-05', NULL, 'пр. Победы, д. 15, кв. 3, Екатеринбург', '5552222', 'Транснефть', NULL, '34567890123', '4567890123456789', 2, 2, 1, 2);
+INSERT INTO doc.patient (id, patient_code, first_name, middle_name, last_name, birth_date, death_date, address, phone_number, work_place_data, bookmark, snils, polis, patient_status_id, passport_id, doctor_id, _user_id) VALUES (1, 1005, 'Елена', 'Петровна', 'Иванова', '1992-03-18', NULL, 'ул. Ленина, д. 20, кв. 8, Санкт-Петербург', '5551111', 'Газпром', NULL, '23456789012', '3456789012345678', 1, 1, NULL, 2);
+INSERT INTO doc.patient (id, patient_code, first_name, middle_name, last_name, birth_date, death_date, address, phone_number, work_place_data, bookmark, snils, polis, patient_status_id, passport_id, doctor_id, _user_id) VALUES (3, 1007, 'Ольга', 'Сергеевна', 'Кузнецова', '1975-06-30', NULL, 'ул. Гагарина, д. 8, кв. 12, Нижний Новгород', '5553333', 'РЖД', NULL, '45678901234', '5678901234567890', 1, 3, 1, 2);
+
+--
+-- Data for Name: block; Type: TABLE DATA; Schema: doc; Owner: postgres
+--
+
+INSERT INTO doc.block (id, name) VALUES (1, 'Разминка');
+INSERT INTO doc.block (id, name) VALUES (2, 'Основной блок');
+INSERT INTO doc.block (id, name) VALUES (3, 'Заминка');
+
+--
+-- Data for Name: exercise_type; Type: TABLE DATA; Schema: doc; Owner: postgres
+--
+
+INSERT INTO doc.exercise_type (id, name) VALUES (1, 'тест1');
+
+
+--
+-- Data for Name: exercise; Type: TABLE DATA; Schema: doc; Owner: postgres
+--
+
+INSERT INTO doc.exercise (id, name, video_url, description, exercise_type_id) VALUES (1, 'тестовое упражнение', 'https://vk.com', NULL, 1);
+INSERT INTO doc.exercise (id, name, video_url, description, exercise_type_id) VALUES (3, 'test', 'test', 'test', NULL);
+
+--
+-- Data for Name: form; Type: TABLE DATA; Schema: doc; Owner: postgres
+--
+
+INSERT INTO doc.form (id, name, description, scale_id) VALUES (1, 'тест анкета 1', 'описание', NULL);
+INSERT INTO doc.form (id, name, description, scale_id) VALUES (2, 'тест анкета 2', 'описание', NULL);
+INSERT INTO doc.form (id, name, description, scale_id) VALUES (3, 'анкета для программы', 'описание', NULL);
