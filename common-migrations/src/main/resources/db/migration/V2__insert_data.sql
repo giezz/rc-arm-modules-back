3fcdbb1226a31fcdcff38fb207a6d2c603a05fb7
giezz <giezz@vk.com>
2024-03-05 00:52:48 +0300

    Обновление схемы бд.

--- common-migrations/src/main/resources/db/migration/V2__insert_data.sql
+++ common-migrations/src/main/resources/db/migration/V2__insert_data.sql
@@ -1,113 +1,164 @@
 --
--- Data for Name: _role; Type: TABLE DATA; Schema: doc; Owner: postgres
+-- TOC entry 3526 (class 0 OID 32440)
+-- Dependencies: 237
+-- Data for Name: scale; Type: TABLE DATA; Schema: anketi; Owner: postgres
 --
 
-INSERT INTO doc._role (id, name) VALUES (1, 'ADMIN');
-INSERT INTO doc._role (id, name) VALUES (2, 'DOCTOR');
+INSERT INTO anketi.scale VALUES (1, 'Шкала A', 'Описание шкалы A');
+INSERT INTO anketi.scale VALUES (2, 'Шкала B', 'Описание шкалы B');
+INSERT INTO anketi.scale VALUES (3, 'Шкала C', 'Описание шкалы C');
 
 --
--- Data for Name: _user; Type: TABLE DATA; Schema: doc; Owner: postgres
+-- TOC entry 3506 (class 0 OID 32327)
+-- Dependencies: 217
+-- Data for Name: form; Type: TABLE DATA; Schema: anketi; Owner: postgres
 --
 
-INSERT INTO doc._user (id, login, password) VALUES (1, 'admin', '$2a$12$XM6RePLH/FcCB1yu3izFYOQRLaiYr/u8F2NrmJwBH/KJbpct1eJQu');
-INSERT INTO doc._user (id, login, password) VALUES (2, 'user', '$2a$12$Kwjwg2cajJef3S/8uCWPwuTg8PAXTDuJ9TPXZ66LVXpegbgSbvsGW');
+INSERT INTO anketi.form VALUES (3, 3, 'Тестовая анкета 3', 'Описание');
+INSERT INTO anketi.form VALUES (1, 1, 'Погода', 'Анкета с двумя вопросами');
+INSERT INTO anketi.form VALUES (2, 2, 'Что Вы ели', 'Анкета с тремя вопросами');
 
 --
--- Data for Name: user_role; Type: TABLE DATA; Schema: doc; Owner: postgres
+-- TOC entry 3522 (class 0 OID 32424)
+-- Dependencies: 233
+-- Data for Name: question; Type: TABLE DATA; Schema: anketi; Owner: postgres
 --
 
-INSERT INTO doc.user_role (_user_id, _role_id) VALUES (1, 2);
-INSERT INTO doc.user_role (_user_id, _role_id) VALUES (1, 1);
-INSERT INTO doc.user_role (_user_id, _role_id) VALUES (2, 1);
-INSERT INTO doc.user_role (_user_id, _role_id) VALUES (2, 2);
+INSERT INTO anketi.question VALUES (1, 'Как у Вас дела?');
+INSERT INTO anketi.question VALUES (2, 'Какая сегодня погода?');
+INSERT INTO anketi.question VALUES (3, 'Что Вы ели на завтрак?');
+INSERT INTO anketi.question VALUES (4, 'Что Вы ели на обед?');
+INSERT INTO anketi.question VALUES (5, 'Что Вы ели на ужин');
 
 --
--- Data for Name: doctor; Type: TABLE DATA; Schema: doc; Owner: postgres
+-- TOC entry 3545 (class 0 OID 32578)
+-- Dependencies: 256
+-- Data for Name: form_question; Type: TABLE DATA; Schema: anketi; Owner: postgres
 --
 
-INSERT INTO doc.doctor (id, doctor_code, first_name, middle_name, last_name, email, phone_number, _user_id) VALUES (1, 100, 'Михаил', 'Алексеевич', 'Батухтин', 'giezz@vk.com', '777777', 1);
-INSERT INTO doc.doctor (id, doctor_code, first_name, middle_name, last_name, email, phone_number, _user_id) VALUES (2, 101, 'Тест', 'Тестович', 'Тестов', 'test@vk.com', '777771', 2);
+INSERT INTO anketi.form_question VALUES (1, 1, 1, '2024-03-04 19:07:15');
+INSERT INTO anketi.form_question VALUES (2, 2, 1, '2024-03-04 19:07:17');
+INSERT INTO anketi.form_question VALUES (3, 3, 2, '2024-03-04 19:07:18');
+INSERT INTO anketi.form_question VALUES (4, 4, 2, '2024-03-04 19:07:19');
+INSERT INTO anketi.form_question VALUES (5, 5, 2, '2024-03-04 19:07:20');
 
 --
--- Data for Name: passport; Type: TABLE DATA; Schema: doc; Owner: postgres
+-- TOC entry 3530 (class 0 OID 32468)
+-- Dependencies: 241
+-- Data for Name: variant; Type: TABLE DATA; Schema: anketi; Owner: postgres
 --
 
-INSERT INTO doc.passport (id, series, number, issued, issued_date) VALUES (1, '2345', '678901', 'Отделение УФМС по г. Санкт-Петербургу', '2012-08-20');
-INSERT INTO doc.passport (id, series, number, issued, issued_date) VALUES (2, '3456', '789012', 'Отделение УФМС по г. Екатеринбургу', '2014-02-10');
-INSERT INTO doc.passport (id, series, number, issued, issued_date) VALUES (3, '4567', '890123', 'Отделение УФМС по г. Нижний Новгороду', '2007-11-05');
-INSERT INTO doc.passport (id, series, number, issued, issued_date) VALUES (4, '5678', '901234', 'Отделение УФМС по г. Казани', '2015-06-15');
-INSERT INTO doc.passport (id, series, number, issued, issued_date) VALUES (5, '6789', '12345 ', 'Отделение УФМС по г. Владивостоку', '2018-03-25');
+INSERT INTO anketi.variant VALUES (6, 2, 1.50, 'Пасмурно');
+INSERT INTO anketi.variant VALUES (4, 2, 4.00, 'Ясная');
+INSERT INTO anketi.variant VALUES (3, 1, 2.50, 'Не очень');
+INSERT INTO anketi.variant VALUES (5, 2, 2.00, 'Местами облачность');
+INSERT INTO anketi.variant VALUES (2, 1, 3.00, 'Хорошо');
+INSERT INTO anketi.variant VALUES (1, 1, 5.00, 'Отлично');
+INSERT INTO anketi.variant VALUES (7, 3, 1.00, 'Кашу');
+INSERT INTO anketi.variant VALUES (8, 3, 0.00, 'Ничего');
+INSERT INTO anketi.variant VALUES (9, 4, 1.00, 'Суп');
+INSERT INTO anketi.variant VALUES (10, 4, 0.00, 'Ничего');
+INSERT INTO anketi.variant VALUES (11, 5, 1.00, 'Рис');
+INSERT INTO anketi.variant VALUES (12, 5, 0.00, 'Ничего');
 
 --
--- Data for Name: patient_status; Type: TABLE DATA; Schema: doc; Owner: postgres
+-- TOC entry 3538 (class 0 OID 32517)
+-- Dependencies: 249
+-- Data for Name: _role; Type: TABLE DATA; Schema: arm; Owner: postgres
 --
 
-INSERT INTO doc.patient_status (id, name) VALUES (1, 'Нуждается в реабилитации');
-INSERT INTO doc.patient_status (id, name) VALUES (2, 'Проходит реабилитацию');
-INSERT INTO doc.patient_status (id, name) VALUES (3, 'Проходил реабилитацию ранее');
+INSERT INTO arm._role VALUES (1, 'ADMIN');
+INSERT INTO arm._role VALUES (2, 'DOCTOR');
 
 --
--- Data for Name: patient; Type: TABLE DATA; Schema: doc; Owner: postgres
+-- TOC entry 3536 (class 0 OID 32508)
+-- Dependencies: 247
+-- Data for Name: _user; Type: TABLE DATA; Schema: arm; Owner: postgres
 --
 
-INSERT INTO doc.patient (id, patient_code, first_name, middle_name, last_name, birth_date, death_date, address, phone_number, work_place_data, bookmark, snils, polis, patient_status_id, passport_id, doctor_id, _user_id) VALUES (4, 1008, 'Иван', 'Дмитриевич', 'Петров', '1988-09-10', NULL, 'пр. Ленинградский, д. 25, кв. 7, Казань', '5554444', 'Лукойл', NULL, '56789012345', '6789012345678901', 3, 4, NULL, 2);
-INSERT INTO doc.patient (id, patient_code, first_name, middle_name, last_name, birth_date, death_date, address, phone_number, work_place_data, bookmark, snils, polis, patient_status_id, passport_id, doctor_id, _user_id) VALUES (5, 1009, 'Мария', 'Андреевна', 'Новикова', '1995-04-27', NULL, 'ул. Красная, д. 18, кв. 6, Владивосток', '5555555', 'Сбербанк', NULL, '67890123456', '7890123456789012', 2, 5, NULL, 2);
-INSERT INTO doc.patient (id, patient_code, first_name, middle_name, last_name, birth_date, death_date, address, phone_number, work_place_data, bookmark, snils, polis, patient_status_id, passport_id, doctor_id, _user_id) VALUES (2, 1006, 'Андрей', 'Валерьевич', 'Сидоров', '1980-12-05', NULL, 'пр. Победы, д. 15, кв. 3, Екатеринбург', '5552222', 'Транснефть', NULL, '34567890123', '4567890123456789', 2, 2, 1, 2);
-INSERT INTO doc.patient (id, patient_code, first_name, middle_name, last_name, birth_date, death_date, address, phone_number, work_place_data, bookmark, snils, polis, patient_status_id, passport_id, doctor_id, _user_id) VALUES (1, 1005, 'Елена', 'Петровна', 'Иванова', '1992-03-18', NULL, 'ул. Ленина, д. 20, кв. 8, Санкт-Петербург', '5551111', 'Газпром', NULL, '23456789012', '3456789012345678', 1, 1, NULL, 2);
-INSERT INTO doc.patient (id, patient_code, first_name, middle_name, last_name, birth_date, death_date, address, phone_number, work_place_data, bookmark, snils, polis, patient_status_id, passport_id, doctor_id, _user_id) VALUES (3, 1007, 'Ольга', 'Сергеевна', 'Кузнецова', '1975-06-30', NULL, 'ул. Гагарина, д. 8, кв. 12, Нижний Новгород', '5553333', 'РЖД', NULL, '45678901234', '5678901234567890', 1, 3, 1, 2);
+INSERT INTO arm._user VALUES (1, 'admin', '$2a$12$XM6RePLH/FcCB1yu3izFYOQRLaiYr/u8F2NrmJwBH/KJbpct1eJQu');
+INSERT INTO arm._user VALUES (2, 'user', '$2a$12$Kwjwg2cajJef3S/8uCWPwuTg8PAXTDuJ9TPXZ66LVXpegbgSbvsGW');
 
 --
--- Data for Name: block; Type: TABLE DATA; Schema: doc; Owner: postgres
+-- TOC entry 3502 (class 0 OID 32305)
+-- Dependencies: 213
+-- Data for Name: doctor; Type: TABLE DATA; Schema: arm; Owner: postgres
 --
 
-INSERT INTO doc.block (id, name) VALUES (1, 'Разминка');
-INSERT INTO doc.block (id, name) VALUES (2, 'Основной блок');
-INSERT INTO doc.block (id, name) VALUES (3, 'Заминка');
+INSERT INTO arm.doctor VALUES (1, 100, 1, 'Михаил', 'Алексеевич', 'Батухтин', 'giezz@vk.com', '777777');
+INSERT INTO arm.doctor VALUES (2, 101, 2, 'Тест', 'Тестович', 'Тестов', 'test@vk.com', '777771');
 
 --
--- Data for Name: exercise_type; Type: TABLE DATA; Schema: doc; Owner: postgres
+-- TOC entry 3520 (class 0 OID 32408)
+-- Dependencies: 231
+-- Data for Name: passport; Type: TABLE DATA; Schema: arm; Owner: postgres
 --
 
+INSERT INTO arm.passport VALUES (1, '2345', '678901', '2012-08-20', 'Отделение УФМС по г. Санкт-Петербургу');
+INSERT INTO arm.passport VALUES (2, '3456', '789012', '2014-02-10', 'Отделение УФМС по г. Екатеринбургу');
+INSERT INTO arm.passport VALUES (3, '4567', '890123', '2007-11-05', 'Отделение УФМС по г. Нижний Новгороду');
+INSERT INTO arm.passport VALUES (4, '5678', '901234', '2015-06-15', 'Отделение УФМС по г. Казани');
+INSERT INTO arm.passport VALUES (5, '6789', '12345 ', '2018-03-25', 'Отделение УФМС по г. Владивостоку');
 
-INSERT INTO doc.scale (name, description)
-VALUES ('Шкала A', 'Описание шкалы A'),
-       ('Шкала B', 'Описание шкалы B'),
-       ('Шкала C', 'Описание шкалы C');
--- Добавление данных в таблицу doc.question
-INSERT INTO doc.question (content)
-VALUES ('Вопрос 1'),
-       ('Вопрос 2'),
-       ('Вопрос 3');
+--
+-- TOC entry 3510 (class 0 OID 32345)
+-- Dependencies: 221
+-- Data for Name: status; Type: TABLE DATA; Schema: arm; Owner: postgres
+--
 
-INSERT INTO doc.exercise_type (name)
-VALUES ('Тип упражнения 1'),
-       ('Тип упражнения 2'),
-       ('Тип упражнения 3');
+INSERT INTO arm.status VALUES (1, 'Нуждается в реабилитации');
+INSERT INTO arm.status VALUES (2, 'Проходит реабилитацию');
+INSERT INTO arm.status VALUES (3, 'Проходил реабилитацию ранее');
 
-INSERT INTO doc.variant (content, score, question_id)
-VALUES ('Вариант 1', 5.0, 1),
-       ('Вариант 2', 3.0, 1),
-       ('Вариант 3', 2.5, 1),
-       ('Вариант 1', 4.0, 2),
-       ('Вариант 2', 2.0, 2),
-       ('Вариант 3', 1.5, 2);
--- Добавление данных в таблицу doc.form
-INSERT INTO doc.form (name, description, scale_id)
-VALUES ('Форма 1', 'Описание формы 1', 1),
-       ('Форма 2', 'Описание формы 2', 2),
-       ('Форма 3', 'Описание формы 3', 3);
+--
+-- TOC entry 3504 (class 0 OID 32316)
+-- Dependencies: 215
+-- Data for Name: patient; Type: TABLE DATA; Schema: arm; Owner: postgres
+--
 
--- Добавление данных в таблицу doc.exercise
-INSERT INTO doc.exercise (name, video_url, description, exercise_type_id)
-VALUES ('Упражнение 1', 'https://www.example.com/video1', 'Описание упражнения 1', 1),
-       ('Упражнение 2', 'https://www.example.com/video2', 'Описание упражнения 2', 2),
-       ('Упражнение 3', 'https://www.example.com/video3', 'Описание упражнения 3', 3);
+INSERT INTO arm.patient VALUES (1, 1008, 2, 4, 3, 'Иван', 'Дмитриевич', 'Петров', '5554444', '56789012345', '6789012345678901', 'm', NULL, '1988-09-10', 'пр. Ленинградский, д. 25, кв. 7, Казань', NULL, 'Лукойл');
+INSERT INTO arm.patient VALUES (2, 1009, 2, 5, 2, 'Мария', 'Андреевна', 'Новикова', '5555555', '67890123456', '7890123456789012', 'f', NULL, '1995-04-27', 'ул. Красная, д. 18, кв. 6, Владивосток', NULL, 'Сбербанк');
+INSERT INTO arm.patient VALUES (3, 1006, 2, 2, 2, 'Андрей', 'Валерьевич', 'Сидоров', '5552222', '34567890123', '4567890123456789', 'm', NULL, '1980-12-05', 'пр. Победы, д. 15, кв. 3, Екатеринбург', NULL, 'Транснефть');
+INSERT INTO arm.patient VALUES (4, 1005, 2, 1, 1, 'Елена', 'Петровна', 'Иванова', '5551111', '23456789012', '3456789012345678', 'f', NULL, '1992-03-18', 'ул. Ленина, д. 20, кв. 8, Санкт-Петербург', NULL, 'Газпром');
+INSERT INTO arm.patient VALUES (5, 1007, 2, 3, 1, 'Ольга', 'Сергеевна', 'Кузнецова', '5553333', '45678901234', '5678901234567890', 'f', NULL, '1975-06-30', 'ул. Гагарина, д. 8, кв. 12, Нижний Новгород', NULL, 'РЖД');
 
+--
+-- TOC entry 3516 (class 0 OID 32375)
+-- Dependencies: 227
+-- Data for Name: block; Type: TABLE DATA; Schema: arm; Owner: postgres
+--
 
+INSERT INTO arm.block VALUES (1, 'Разминка');
+INSERT INTO arm.block VALUES (2, 'Основной блок');
+INSERT INTO arm.block VALUES (3, 'Заминка');
 
--- Добавление связей между формами и вопросами в таблицу doc.form_question
-INSERT INTO doc.form_question (form_id, question_id)
-VALUES (1, 1),
-       (2, 2),
-       (2, 3),
-       (3, 3);
+--
+-- TOC entry 3528 (class 0 OID 32454)
+-- Dependencies: 239
+-- Data for Name: exercise_type; Type: TABLE DATA; Schema: arm; Owner: postgres
+--
+
+INSERT INTO arm.exercise_type VALUES (1, 'Тип упражнения 1');
+INSERT INTO arm.exercise_type VALUES (2, 'Тип упражнения 2');
+INSERT INTO arm.exercise_type VALUES (3, 'Тип упражнения 3');
+
+--
+-- TOC entry 3508 (class 0 OID 32336)
+-- Dependencies: 219
+-- Data for Name: exercise; Type: TABLE DATA; Schema: arm; Owner: postgres
+--
+
+INSERT INTO arm.exercise VALUES (1, 1, 'https://www.example.com/video1', 'Упражнение 1', 'Описание упражнения 1');
+INSERT INTO arm.exercise VALUES (2, 2, 'https://www.example.com/video2', 'Упражнение 2', 'Описание упражнения 2');
+INSERT INTO arm.exercise VALUES (3, 3, 'https://www.example.com/video3', 'Упражнение 3', 'Описание упражнения 3');
+
+--
+-- TOC entry 3539 (class 0 OID 32528)
+-- Dependencies: 250
+-- Data for Name: user_role; Type: TABLE DATA; Schema: arm; Owner: postgres
+--
+
+INSERT INTO arm.user_role VALUES (1, 2);
+INSERT INTO arm.user_role VALUES (1, 1);
+INSERT INTO arm.user_role VALUES (2, 1);
+INSERT INTO arm.user_role VALUES (2, 2);
