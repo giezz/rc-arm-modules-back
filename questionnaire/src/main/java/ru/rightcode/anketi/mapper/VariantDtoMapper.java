e933009e44b3d764974d6c954cb6e0a9a0df836a
SoundFlog <paha-vershinin@mail.ru>
2024-03-05 12:04:25 +0300

    удаление логики переиспользования вариантов

--- questionnaire/src/main/java/ru/rightcode/anketi/mapper/VariantDtoMapper.java
+++ questionnaire/src/main/java/ru/rightcode/anketi/mapper/VariantDtoMapper.java
@@ -3,29 +3,16 @@ package ru.rightcode.anketi.mapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import ru.rightcode.anketi.dto.VariantDto;
-import ru.rightcode.anketi.exception.NotFoundException;
 import ru.rightcode.anketi.model.Variant;
-import ru.rightcode.anketi.repository.VariantRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class VariantDtoMapper implements Mapper<VariantDto, Variant> {
-    private final VariantRepository variantRepository;
     @Override
     public Variant toEntity(VariantDto variantDto) {
-        if (variantDto.getId() != null){
-            Optional<Variant> variant = variantRepository.findById(variantDto.getId());
-            if (variant.isPresent()){
-                return variant.get();
-            }else{
-                throw new NotFoundException("Variant not found with id: "+variantDto.getId());
-            }
-        }
         Variant variant = new Variant();
-        variant.setId(variantDto.getId());
         variant.setContent(variantDto.getContent());
         variant.setScore(variantDto.getScore());
         return variant;
