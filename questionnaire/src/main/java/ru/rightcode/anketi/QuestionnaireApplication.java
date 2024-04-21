aa163f285bfe93404af48cbe393037dc8656e6b0
SoundFlog <paha-vershinin@mail.ru>
2024-04-21 22:56:32 +0300

    before docker

--- questionnaire/src/main/java/ru/rightcode/anketi/QuestionnaireApplication.java
+++ questionnaire/src/main/java/ru/rightcode/anketi/QuestionnaireApplication.java
@@ -2,8 +2,10 @@ package ru.rightcode.anketi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "ru.rightcode.anketi.repository")
 public class QuestionnaireApplication {
 
     public static void main(String[] args) {
