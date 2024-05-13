ccc8a22eaa6db4af621b8ebd2942973b4360be13
giezz <giezz@vk.com>
2024-05-13 18:30:50 +0300

    Операции над объектами вынесены в соответствующие классы.

--- arm/src/main/java/ru/rightcode/arm/service/ProgramFormService.java
+++ arm/src/main/java/ru/rightcode/arm/service/ProgramFormService.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.rightcode.arm.dto.response.ProgramFormResultResponse;
 import ru.rightcode.arm.mapper.ProgramFormResultResponseMapper;
+import ru.rightcode.arm.model.Form;
 import ru.rightcode.arm.model.ProgramForm;
+import ru.rightcode.arm.model.Type;
 import ru.rightcode.arm.repository.ProgramFormRepository;
 
 import java.util.List;
@@ -22,5 +24,13 @@ public class ProgramFormService {
         List<ProgramForm> results = programFormRepository.findFormResultsByRehabProgramId(programId, excludeIds);
 
         return results.stream().map(programFormResultResponseMapper::map).toList();
+    }
+
+    public ProgramForm create(Form form, Type type) {
+        ProgramForm programForm = new ProgramForm();
+        programForm.setForm(form);
+        programForm.setType(type);
+
+        return  programForm;
     }
 }
