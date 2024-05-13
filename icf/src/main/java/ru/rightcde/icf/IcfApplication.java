9b12a87f13b1e11a53cbfffc83734da96de840d1
giezz <giezz@vk.com>
2024-05-13 19:34:36 +0300

    Добавлен модуль МКФ (icf).

---
+++ icf/src/main/java/ru/rightcde/icf/IcfApplication.java
@@ -0,0 +1,13 @@
+package ru.rightcde.icf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcfApplication.class, args);
+    }
+
+}
---
+++ icf/src/test/java/ru/rightcde/icf/IcfApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rightcde.icf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
