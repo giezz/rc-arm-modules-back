17e40093c1e669c61a06297451ba37d8a1f57816
giezz <giezz@vk.com>
2024-05-17 02:11:34 +0300

    URL вынесен в конфиг.

--- arm/src/main/java/ru/rightcode/arm/ArmApplication.java
+++ arm/src/main/java/ru/rightcode/arm/ArmApplication.java
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ArmApplication {
 
-    public static final String MEDCART_API_URL = "http://localhost:8081/api/v1";
-
     public static void main(String[] args) {
         SpringApplication.run(ArmApplication.class, args);
     }
+
 }
