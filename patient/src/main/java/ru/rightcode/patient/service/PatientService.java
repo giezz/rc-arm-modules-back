29128004762d00ddb21404c90f00ccb6376edd07
Soundflog <paha-vershinin@mail.ru>
2024-05-19 13:11:34 +0300

    Получение программы и доктора [WARNING]
    проверка без данных

--- patient/src/main/java/ru/rightcode/patient/service/PatientService.java
+++ patient/src/main/java/ru/rightcode/patient/service/PatientService.java
@@ -3,12 +3,12 @@ package ru.rightcode.patient.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import ru.rightcode.patient.dto.PatientInfo;
+import ru.rightcode.patient.dto.response.DoctorResponse;
 import ru.rightcode.patient.dto.response.PatientResponse;
+import ru.rightcode.patient.dto.response.RehabProgramResponse;
 import ru.rightcode.patient.exception.NotFoundException;
 import ru.rightcode.patient.mapper.PatientResponseMapper;
 import ru.rightcode.patient.model.Patient;
-import ru.rightcode.patient.model.User;
 import ru.rightcode.patient.repository.PatientRepository;
 
 @Service
@@ -18,15 +18,31 @@ public class PatientService {
     private final PatientRepository patientRepository;
     private final PatientResponseMapper patientResponseMapper;
 
-    public PatientResponse getYourSelf(String username){
-        Patient patientFromDB = patientRepository.findByUserUsername(username, Patient.class)
+    private final RehabProgramService rehabProgramService;
+
+    @Transactional
+    public Patient getPatientByUsername(String username) {
+        return patientRepository.getPatientByUserUsername(username)
                 .orElseThrow(() -> new NotFoundException("Пациент не найден"));
-        return patientResponseMapper.toResponse(patientFromDB);
+    }
+
+    public PatientResponse getYourSelf(String username){
+        return patientResponseMapper.toResponse(getPatientByUsername(username));
+    }
+
+    // TODO: doctor
+    public DoctorResponse getDoctor(String username){
+        return rehabProgramService.getDoctorResponseByPatient(getPatientByUsername(username));
     }
 
     // TODO: История проведения реабилитаций
     // TODO: Результаты реабилитаций
     // TODO: ПРограмма реабилитации
+    @Transactional
+    public RehabProgramResponse getRehabProgram(String username){
+        Patient patientFromDB = getPatientByUsername(username);
+        return rehabProgramService.getRehabProgramResponseByPatient(patientFromDB);
+    }
     // TODO: Модули реабилитации
     // TODO: get Эпикриз
     // TODO: get Вводная анкета
