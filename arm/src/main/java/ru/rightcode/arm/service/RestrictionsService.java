d734122229a662800b7af06c95c6a41ee6539087
giezz <giezz@vk.com>
2024-05-20 19:55:01 +0300

    Обновлена логика проверки ограничений.

--- arm/src/main/java/ru/rightcode/arm/service/RestrictionsService.java
+++ arm/src/main/java/ru/rightcode/arm/service/RestrictionsService.java
@@ -3,18 +3,19 @@ package ru.rightcode.arm.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import ru.rightcode.arm.repository.DoctorRepository;
-import ru.rightcode.arm.repository.PatientRepository;
-import ru.rightcode.arm.repository.RehabProgramRepository;
+import ru.rightcode.arm.model.RehabProgram;
 
 @Transactional(readOnly = true)
 @Service
 @RequiredArgsConstructor
 public class RestrictionsService {
 
-    private final RehabProgramRepository rehabProgramRepository;
+    private final DoctorService doctorService;
 
-    public boolean canDoctorEditRehabProgram(Long doctorId, Long rehabProgramId) {
-        return rehabProgramRepository.checkIfDoctorCanEdit(doctorId, rehabProgramId);
+    public boolean canDoctorEditRehabProgram(RehabProgram rehabProgram, String login) {
+        Long doctorId = doctorService.getDoctorIdByLogin(login).getId();
+
+        return rehabProgram.canDoctorEdit(doctorId) && rehabProgram.getIsCurrent();
     }
+
 }
