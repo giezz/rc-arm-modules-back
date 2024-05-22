3eacf5f11a045ca94cb0f3c563c1442ccf2bdb47
Soundflog <paha-vershinin@mail.ru>
2024-05-22 15:27:56 +0300

    add service Module

---
+++ patient/src/main/java/ru/rightcode/patient/service/ModuleService.java
@@ -0,0 +1,28 @@
+package ru.rightcode.patient.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.rightcode.patient.dto.response.module.ModuleResponse;
+import ru.rightcode.patient.exception.NotFoundException;
+import ru.rightcode.patient.mapper.module.ModuleResponseMapper;
+import ru.rightcode.patient.model.Module;
+import ru.rightcode.patient.repository.ModuleRepository;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ModuleService {
+    private final ModuleRepository repository;
+    private final ModuleResponseMapper moduleResponseMapper;
+
+    @Transactional
+    protected Module findById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Module with id " + id + " not found"));
+    }
+
+    public ModuleResponse getModuleResponse(Long moduleId) {
+        return moduleResponseMapper.toModuleResponse(findById(moduleId));
+    }
+}
