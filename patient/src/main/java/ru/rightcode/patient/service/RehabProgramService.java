5e12a14819f30365e3f4c2a3051953ce74e047c5
Soundflog <paha-vershinin@mail.ru>
2024-05-22 15:24:10 +0300

    del doctor from service

--- patient/src/main/java/ru/rightcode/patient/service/RehabProgramService.java
+++ patient/src/main/java/ru/rightcode/patient/service/RehabProgramService.java
@@ -1,14 +1,12 @@
 package ru.rightcode.patient.service;
 
-import com.sun.jdi.request.ExceptionRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import ru.rightcode.patient.dto.response.DoctorResponse;
 import ru.rightcode.patient.dto.response.RehabProgramResponse;
 import ru.rightcode.patient.exception.NotFoundException;
 import ru.rightcode.patient.mapper.DoctorResponseMapper;
-import ru.rightcode.patient.mapper.RehabProgramMapper;
+import ru.rightcode.patient.mapper.rehab.RehabProgramMapper;
 import ru.rightcode.patient.model.Patient;
 import ru.rightcode.patient.model.RehabProgram;
 import ru.rightcode.patient.repository.RehabProgramRepository;
@@ -29,13 +27,6 @@ public class RehabProgramService {
         return rehabProgramRepository.findByPatient(patient);
     }
 
-    // Получение программы реабилитации по пациенту для получения доктора
-    @Transactional
-    protected RehabProgram getDoctorByPatient(Patient patient) {
-        return rehabProgramRepository.findDoctorByPatient(patient)
-                .orElseThrow(() -> new NotFoundException("Doctor not found"));
-    }
-
     // Проверка актуальности программы реабилитации
     private RehabProgram checkRehabProgramIsCurrent(RehabProgram rehabProgram) {
         if (!rehabProgram.getIsCurrent()) {
@@ -44,15 +35,9 @@ public class RehabProgramService {
         return rehabProgram;
     }
 
+//    @Cacheable(value = "RehabProgramService::getRehabProgramResponseByPatient", key = "#patient.id")
     public RehabProgramResponse getRehabProgramResponseByPatient(Patient patient) {
         return rehabProgramMapper.toRehabProgramResponse(getRehabProgramByPatient(patient));
     }
 
-    @Transactional
-    public DoctorResponse getDoctorResponseByPatient(Patient patient) {
-        return doctorResponseMapper.toResponse(
-                checkRehabProgramIsCurrent(
-                        getDoctorByPatient(patient)
-                ).getDoctor());
-    }
 }
