49389febc0a8b9b720f2a0d80f27f7cebc450481
Soundflog <paha-vershinin@mail.ru>
2024-05-22 15:24:39 +0300

    add getModule

--- patient/src/main/java/ru/rightcode/patient/service/PatientService.java
+++ patient/src/main/java/ru/rightcode/patient/service/PatientService.java
@@ -3,8 +3,8 @@ package ru.rightcode.patient.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import ru.rightcode.patient.dto.response.DoctorResponse;
 import ru.rightcode.patient.dto.response.PatientResponse;
+import ru.rightcode.patient.dto.response.module.ModuleResponse;
 import ru.rightcode.patient.dto.response.RehabProgramResponse;
 import ru.rightcode.patient.exception.NotFoundException;
 import ru.rightcode.patient.mapper.PatientResponseMapper;
@@ -19,20 +19,17 @@ public class PatientService {
     private final PatientResponseMapper patientResponseMapper;
 
     private final RehabProgramService rehabProgramService;
+    private final ModuleService moduleService;
 
     @Transactional
-    public Patient getPatientByUsername(String username) {
+    protected Patient getPatientByUsername(String username) {
         return patientRepository.getPatientByUserUsername(username)
                 .orElseThrow(() -> new NotFoundException("Пациент не найден"));
     }
 
+//    @Cacheable(value = "PatientService::getYourSelf", key = "#username")
     public PatientResponse getYourSelf(String username){
         return patientResponseMapper.toResponse(getPatientByUsername(username));
-    }
-
-    // TODO: doctor
-    public DoctorResponse getDoctor(String username){
-        return rehabProgramService.getDoctorResponseByPatient(getPatientByUsername(username));
     }
 
     // TODO: История проведения реабилитаций
@@ -43,7 +40,13 @@ public class PatientService {
         Patient patientFromDB = getPatientByUsername(username);
         return rehabProgramService.getRehabProgramResponseByPatient(patientFromDB);
     }
+
     // TODO: Модули реабилитации
+    @Transactional
+    public ModuleResponse getModule(String username, Long moduleId){
+        Patient patientFromDB = getPatientByUsername(username);
+        return moduleService.getModuleResponse(moduleId);
+    }
     // TODO: get Эпикриз
     // TODO: get Вводная анкета
     // TODO: get Выходная анкета
