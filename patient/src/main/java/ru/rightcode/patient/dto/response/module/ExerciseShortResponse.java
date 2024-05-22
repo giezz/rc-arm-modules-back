3b5d35095adce0cad2cde5b92f50937e5dec010b
Soundflog <paha-vershinin@mail.ru>
2024-05-23 00:01:15 +0300

    add id

--- patient/src/main/java/ru/rightcode/patient/dto/response/module/ExerciseShortResponse.java
+++ patient/src/main/java/ru/rightcode/patient/dto/response/module/ExerciseShortResponse.java
@@ -3,6 +3,7 @@ package ru.rightcode.patient.dto.response.module;
 import java.io.Serializable;
 
 public record ExerciseShortResponse(
+        Long id,
         String name,
         String exerciseType,
         String description,
--- patient/src/main/java/ru/rightcode/patient/dto/response/module/FormShortResponse.java
+++ patient/src/main/java/ru/rightcode/patient/dto/response/module/FormShortResponse.java
@@ -3,6 +3,7 @@ package ru.rightcode.patient.dto.response.module;
 import java.io.Serializable;
 
 public record FormShortResponse(
+        Long id,
         String name,
         String description
 ) implements Serializable {
