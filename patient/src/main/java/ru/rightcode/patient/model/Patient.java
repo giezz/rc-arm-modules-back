0677213e91d50973d408f9bda7386daaa88ebe0e
Soundflog <paha-vershinin@mail.ru>
2024-05-23 10:28:58 +0300

    reference List --> Set

--- patient/src/main/java/ru/rightcode/patient/model/Patient.java
+++ patient/src/main/java/ru/rightcode/patient/model/Patient.java
@@ -7,10 +7,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
 @NoArgsConstructor
 @Getter
@@ -98,6 +100,21 @@ public class Patient {
     private Passport passport;
 
     @OneToMany(mappedBy = "patient", fetch = FetchType.LAZY)
-    private List<RehabProgram> rehabPrograms = new ArrayList<>();
+    private Set<RehabProgram> rehabPrograms = new HashSet<>();
 
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Patient patient = (Patient) o;
+        return getId() != null && Objects.equals(getId(), patient.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
