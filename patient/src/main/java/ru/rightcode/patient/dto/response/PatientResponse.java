df3a73daadae87881090ce6d52f73c2c04b59e72
Soundflog <paha-vershinin@mail.ru>
2024-05-27 19:47:31 +0300

    del passport from patientResponse

--- patient/src/main/java/ru/rightcode/patient/dto/response/PatientResponse.java
+++ patient/src/main/java/ru/rightcode/patient/dto/response/PatientResponse.java
@@ -8,16 +8,12 @@ public record PatientResponse(
         String firstName,
         String middleName,
         String lastName,
-        String gender,
         LocalDate birthDate,
         String address,
         String phoneNumber,
         String workPlaceData,
-        String snils,
-        String polis,
         String bookmark,
 
-        String patientStatus, // TODO: сделать ResponseEntity
-        PassportResponse passport // TODO: сделать ResponseEntity
+        String patientStatus // TODO: сделать ResponseEntity
 ) implements Serializable {
 }
