48edc6988257fba90d8e36a5ada5849060078098
Soundflog <paha-vershinin@mail.ru>
2024-05-29 16:43:57 +0300

    [P] update Test submit answers

--- patient/src/test/java/ru/rightcode/patient/service/FormSubmitTest.java
+++ patient/src/test/java/ru/rightcode/patient/service/FormSubmitTest.java
@@ -3,25 +3,22 @@ package ru.rightcode.patient.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import ru.rightcode.patient.dto.request.AnswerRequest;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 @SpringBootTest
-@ActiveProfiles("test")
-public class ModuleFormServiceTest {
-
+public class FormSubmitTest {
     @Autowired
     private ModuleFormService moduleFormService;
 
     @Test
-    void completeForm() {
+    public void submitAnswers(){
         List<AnswerRequest> request = List.of(
-                new AnswerRequest(2L, null),
-                new AnswerRequest(5L, BigDecimal.valueOf(2.0))
+                new AnswerRequest(35L, null),
+                new AnswerRequest(37L, BigDecimal.valueOf(2.0))
         );
-        moduleFormService.submitModuleFormAnswer(1L, request);
+        moduleFormService.submitModuleFormAnswer(2L, request);
     }
 }
