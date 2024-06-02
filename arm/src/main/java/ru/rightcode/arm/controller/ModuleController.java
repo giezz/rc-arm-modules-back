f1e3e3693c7813ae258826d4e5c72d04d2764001
giezz <giezz@vk.com>
2024-06-02 15:22:40 +0300

    Редактирование модуля теперь через @PutMapping.

--- arm/src/main/java/ru/rightcode/arm/controller/ModuleController.java
+++ arm/src/main/java/ru/rightcode/arm/controller/ModuleController.java
@@ -1,14 +1,10 @@
 package ru.rightcode.arm.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.rightcode.arm.dto.request.AddModuleExerciseRequest;
-import ru.rightcode.arm.dto.request.AddModuleFormRequest;
 import ru.rightcode.arm.dto.request.RenameModuleRequest;
 import ru.rightcode.arm.dto.request.UpdateModuleRequest;
-import ru.rightcode.arm.dto.response.ModuleDetailsResponse;
 import ru.rightcode.arm.service.ModuleService;
 
 import java.security.Principal;
@@ -31,40 +27,6 @@ public class ModuleController {
                                     @RequestBody RenameModuleRequest request,
                                     Principal principal) {
         return ResponseEntity.ok(moduleService.renameModule(principal.getName(), request, id));
-    }
-
-    @PostMapping("/{id}/form")
-    public ResponseEntity<?> addForm(@PathVariable Long id,
-                                     @RequestBody AddModuleFormRequest request,
-                                     Principal principal) {
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(moduleService.addForm(principal.getName(), request, id));
-    }
-
-    @PostMapping("/{id}/exercise")
-    public ResponseEntity<?> addExercise(@PathVariable Long id,
-                                         @RequestBody AddModuleExerciseRequest request,
-                                         Principal principal) {
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(moduleService.addExercise(principal.getName(), request, id));
-    }
-
-    @DeleteMapping("/{id}/form/{moduleFormId}")
-    public ResponseEntity<?> deleteForm(@PathVariable("id") Long moduleId,
-                                        @PathVariable Long moduleFormId,
-                                        Principal principal) {
-        return ResponseEntity
-                .ok(moduleService.deleteForm(principal.getName(), moduleId, moduleFormId));
-    }
-
-    @DeleteMapping("/{id}/exercise/{moduleExerciseId}")
-    public ResponseEntity<?> deleteExercise(@PathVariable("id") Long moduleId,
-                                            @PathVariable Long moduleExerciseId,
-                                            Principal principal) {
-        return ResponseEntity
-                .ok(moduleService.deleteExercise(principal.getName(), moduleId, moduleExerciseId));
     }
 
     @PutMapping("/{id}")
