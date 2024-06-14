d8c77e800e0ab864736a72dfb399f670809bb220
giezz <giezz@vk.com>
2024-06-14 13:44:44 +0300

    Подправлен совет logControllerMethods.

--- arm/src/main/java/ru/rightcode/arm/aspect/LoggingAspect.java
+++ arm/src/main/java/ru/rightcode/arm/aspect/LoggingAspect.java
@@ -1,21 +1,19 @@
 package ru.rightcode.arm.aspect;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Aspect
 @Component
+@Slf4j
 public class LoggingAspect {
-
-    private static final Logger log = LoggerFactory.getLogger(LoggingAspect.class);
 
     @Pointcut("@within(ru.rightcode.arm.annotation.Loggable)")
     public void controllerMethods() {}
@@ -23,16 +21,25 @@ public class LoggingAspect {
     @Before("controllerMethods()")
     public void logControllerMethods(JoinPoint joinPoint) {
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-        HttpServletRequest request;
         String className = joinPoint.getSignature().getDeclaringTypeName();
         String methodName = joinPoint.getSignature().getName();
         if (attributes == null) {
             log.info("Request attributes are null, Controller: {}, Method: {}", className, methodName);
             return;
         }
-        request = attributes.getRequest();
-        String ipAddr = request.getRemoteAddr();
+        HttpServletRequest request = attributes.getRequest();
+        String ipAddr = getIpAddress(request);
         String userLogin = request.getUserPrincipal().getName();
         log.info("IPAddr: {}, User: {}, Controller: {}, Method: {}", ipAddr, userLogin, className, methodName);
     }
+
+    private String getIpAddress(HttpServletRequest request) {
+        String ipAddr = request.getHeader("X-Forwarded-For");
+        if (ipAddr == null){
+            ipAddr = request.getRemoteAddr();
+        }
+
+        return ipAddr;
+    }
+
 }
